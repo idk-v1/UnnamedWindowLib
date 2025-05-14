fbd803504895248abb628a7c85a79237d0f9fbbb
idk-v1 <benhamilton.jpg@gmail.com>
2025-05-14 12:54:09 -0500

    more empty shells

---
+++ unw/unw.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "unw_file.h"
+#include "unw_image.h"
+#include "unw_input.h"
+#include "unw_window.h"
+
+#include "unw_util.h"
+#include "util/unw_string.h"
+#include "util/unw_mem.h"
+#include "util/unw_time.h"
--- unw/unw_util.h
+++ unw/unw_util.h
@@ -1,3 +1,23 @@
 #pragma once
-#include "util/unw_string.h"
-#include "util/unw_memory.h"
+
+#ifdef __cplusplus
+#define UNWFN extern "C"
+#else
+#define UNWFN
+#endif
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef uint8_t unw_byte;
+
+typedef struct unw_vec2
+{
+	int64_t x, y;
+} unw_vec2;
+
+typedef struct unw_rect
+{
+	int64_t x, y, w, h;
+} unw_rect;
