86d7a61e7b379962c786da504ffb5f6afc609a8d
idk-v1 <benhamilton.jpg@gmail.com>
2025-05-16 12:05:49 -0500

    main design

--- idk/idk_time.h
+++ idk/idk_time.h
@@ -9,6 +9,7 @@ idk_Time idk_time_now();
 idk_Time idk_time_diff(idk_Time old);
 
 bool idk_time_timer(idk_Time* old, idk_Time duration);
+bool idk_time_timerLoop(idk_Time* old, idk_Time duration);
 
 void idk_time_sleep(idk_Time duration);
 
--- main.c
+++ main.c
@@ -1,17 +1,29 @@
 #include "idk/idk_window.h"
 #include "idk/idk_input.h"
+#include "idk/idk_time.h"
 
 int main(int argc, char* argv[])
 {
 	idk_HWindow window = idk_window("test", 800, 600, -1, -1, 0);
+
+	idk_Time lastTime = idk_time_now();
+	idk_Time fpsTime = idk_time_now();
 
 	while (idk_window_isOpen(window))
 	{
 		idk_window_updateAll();
 		idk_input_update();
 
-		idk_image_fill(idk_window_getFrameBuf(window), idk_color(0, 0, 0, 255));
-		idk_window_display(window);
+		while (idk_time_timerLoop(&lastTime, 1000 / 60))
+		{
+			// update
+		}
+
+		if (idk_time_timer(&fpsTime, 1000 / 60))
+		{
+			idk_image_fill(idk_window_getFrameBuf(window), idk_color(0, 0, 0, 255));
+			idk_window_display(window);
+		}
 	}
 
 	idk_window_close(window);
